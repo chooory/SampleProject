4248aa93aa79b8c647f1131ba833684f93bdf664
bora121111 <bora121111@naver.com>
2023-09-13 12:21:49 +0900

    핫픽스 커밋

--- SamplePro/src/main/java/MainServlet.java
+++ SamplePro/src/main/java/MainServlet.java
@@ -15,7 +15,7 @@ public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("mainServlet");
+		System.out.println("mainServlet2");
 	}
 
 	/**
